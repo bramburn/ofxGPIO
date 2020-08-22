657388d03dd742300ccd1f6b266c18ae8aa5dc37
Bhavesh Ramburn <nitrogen@gmail.com>
2020-08-22 12:16:39 +0100

    add ofxgpio

--- example-i2c/src/main.cpp
+++ example-i2c/src/main.cpp
@@ -2,30 +2,30 @@
 #include "ofAppNoWindow.h"
 #include "ofxGPIO.h"
 
-class ofApp : public ofBaseApp{
-        public:
-		I2c * bus;
+class ofApp : public ofBaseApp {
+public:
+    I2c *bus;
 
-                void setup(){
-			bus = new I2c("/dev/i2c-1");
-			bus->addressSet(0x04);
+    void setup() {
+        bus = new I2c("/dev/i2c-1");
+        bus->addressSet(0x04);
 
-		}
+    }
 
-                void update(){
-	                bus->writeByte(0x04,1);
-			usleep(500000);
-	                bus->writeByte(0x04,0);
-			usleep(500000);
-		}
+    void update() {
+        bus->writeByte(0x04, 1);
+        usleep(500000);
+        bus->writeByte(0x04, 0);
+        usleep(500000);
+    }
 
-                void draw(){
+    void draw() {
 
-		}
+    }
 };
 
-int main( ){
-	ofAppNoWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);
-	ofRunApp( new ofApp());
+int main() {
+    ofAppNoWindow window;
+    ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
+    ofRunApp(new ofApp());
 }
--- example-servo/src/main.cpp
+++ example-servo/src/main.cpp
@@ -2,35 +2,35 @@
 #include "ofAppNoWindow.h"
 #include "ofxGPIO.h"
 
-class noWin : public ofBaseApp{
-        public:
+class noWin : public ofBaseApp {
+public:
 
-	        rpiPWM1 *pwm;
-		int count;
+    rpiPWM1 *pwm;
+    int count;
 
-                void setup(){
-			pwm = new rpiPWM1(1000.0, 256, 80.0, rpiPWM1::MSMODE);
-			count=0;			
-                }
-		
-                void update(){
-			pwm->setDutyCycleCount(count); 
-	                count += 16;
-			if(count>100)
-				count=0;
-			printf("Duty Cycle is %3.2lf \n",pwm->getDutyCycle());
-		        printf("Divisor is %d\n", pwm->getDivisor());
-		        usleep(2000000);
-                }
+    void setup() {
+        pwm = new rpiPWM1(1000.0, 256, 80.0, rpiPWM1::MSMODE);
+        count = 0;
+    }
 
-                void exit(){
+    void update() {
+        pwm->setDutyCycleCount(count);
+        count += 16;
+        if (count > 100)
+            count = 0;
+        printf("Duty Cycle is %3.2lf \n", pwm->getDutyCycle());
+        printf("Divisor is %d\n", pwm->getDivisor());
+        usleep(2000000);
+    }
 
-                }
+    void exit() {
+
+    }
 
 };
 
-int main( ){
-        ofAppNoWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);
-	ofRunApp( new noWin() );
+int main() {
+    ofAppNoWindow window;
+    ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
+    ofRunApp(new noWin());
 }
